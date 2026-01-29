4f959af399f88d728a5594696aa4cb5d9c9b7066
ESchwenker <Eschwenker24@outlook.com>
2026-01-29 16:25:55 -0500

    start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import Start from './pages/Start/Start'
+import { Routes, Route } from 'react-router-dom'
+
 
 const App = () => {
   return (
     <div>
-      <Home />
-
-      
+      <Routes>
+        <Route path='/' element={<Start/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Start/Start.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Start.css'
+import popcorn_logo from '../../assets/popcorn_logo.png'
+import popcorn_banner from '../../assets/popcorn_banner.jpg'
+
+function Start () {
+  return (
+    <div className="start">
+          <div className="nav-start">
+            <div className="nav__container-start">
+              <div className="nav__info-start">
+                <figure className="nav__logo-start">
+                    <img className="nav__img-start" src={popcorn_logo}/>
+                    <h2 className="title-start">PopcornPix</h2>
+                </figure>
+                <div className="nav__links-start">
+                    <a href="#" className="nav__link-start">Home</a>
+                    <a href="#" className="nav__link-start">Find Your Flick</a>
+                    <button className="nav__btn-start" a href="#">Contact</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div className="start__info">
+            <div className="start__description">America's #1 site to search for the movies everyone knows and loves.</div>
+            <p className="start__tag">ROLL THE REELS... AND START SEARCHING!</p>
+            <div className="start__search">
+              <input className="start__input" type="text" placeholder="Find your flix with PopcornPix!"/>
+              <button className="start__btn">Roll Film</button>
+            </div>
+          </div>
+          <footer>
+            <img className="footer__img" src={popcorn_banner} alt="popcorn"/>
+          </footer>
+    </div>
+  )
+}
+
+export default Start
